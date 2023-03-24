1ca77d5fba127f7016b589f34d0a699c8a280e04
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-03-24 18:49:16 -0400

    Add function for getting user from id

--- mongodb_db.py
+++ mongodb_db.py
@@ -1,11 +1,19 @@
 import pymongo
 
+# create a MongoDB client and connect to the database
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["mydatabase"]
+
+# define the collection and document structure
+users = db["users"]
+
 
 def add_history(user_id, message):
     query = {"_id": user_id}
-    update = {"$push": {"history": message}}
-    result = users.update_one(query, update)
-    print("Updated", result.modified_count, "document(s)")
+    update = {"$set": {"history": message}}
+    result = users.find_one_and_update(query, update, upsert=True)
+    print("ok")
+    # print("Updated", result.modified_count, "document(s)")
 
 
 # define a function for getting the user id based on phone number
@@ -32,14 +40,11 @@ def add_user(phone_number, is_active=False, history=None):
     return result.inserted_id
 
 
+def get_user(user_id):
+    return users.find_one({"_id": user_id})
+
+
 if __name__ == "__main__":
-    # create a MongoDB client and connect to the database
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["mydatabase"]
-
-    # define the collection and document structure
-    users = db["users"]
-
     # Add user
     user = {"phone_number": "1234567890", "is_active": True, "history": None}
     _ = add_user(**user)
@@ -49,3 +54,6 @@ if __name__ == "__main__":
     # call the add_history function to update the user's history field
     add_history(user_id, "User created")
     add_history(user_id, "User logged in")
+
+    res = get_user(user_id)
+    print(res)
