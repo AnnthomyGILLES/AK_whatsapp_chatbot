c9a4be98df81724c3a15a6197f0fb2b02dcd0bdb
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-03-24 16:23:05 -0400

    Add new user

--- create_db.py
+++ create_db.py
@@ -1,35 +1,37 @@
 import sqlite3
+from contextlib import closing
 
-db_name = "users.db"
-conn = sqlite3.connect(db_name)
+DB_NAME = "users.db"
 
 
 def create_db():
-    try:
-        cursor = conn.cursor()
-        print("Database created!")
+    with closing(sqlite3.connect(DB_NAME)) as conn:
+        with closing(conn.cursor()) as cursor:
+            try:
+                # Create operation
+                create_users = """CREATE TABLE IF NOT EXISTS users (
+              id INTEGER PRIMARY KEY,
+              phone_number TEXT NOT NULL,
+              is_active INTEGER NOT NULL);
+              """
+                cursor.execute(create_users)
 
-        # Create operation
-        create_query = """CREATE TABLE IF NOT EXISTS users(
-      id INTEGER PRIMARY KEY,
-      phone_number TEXT NOT NULL,
-      active INTEGER NOT NULL, 
-      history_log TEXT);
-      """
-        cursor.execute(create_query)
-        print("Table created!")
+                create_history = """CREATE TABLE IF NOT EXISTS history (
+                id INTEGER PRIMARY KEY,
+                user_id INTEGER NOT NULL, 
+                history_log TEXT NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users (id));
+                """
+                cursor.execute(create_history)
+                print("Table created!")
 
-    except sqlite3.Error as e:
-        # Handle errors
-        print(f"Error creating table: {e}")
-
-    finally:
-        # Close the connection
-        conn.close()
+            except sqlite3.Error as e:
+                # Handle errors
+                print(f"Error creating table: {e}")
 
 
-def insert_command(conn, phone_number, active, history_log):
-    command = "INSERT INTO student VALUES (?, ?, ?)"
+def insert_command(conn, phone_number, active):
+    command = "INSERT INTO users VALUES (?, ?)"
     cur = conn.cursor()
     cur.execute("BEGIN")
     try:
@@ -38,7 +40,6 @@ def insert_command(conn, phone_number, active, history_log):
             (
                 phone_number,
                 active,
-                history_log,
             ),
         )
         cur.execute("COMMIT")
@@ -48,5 +49,29 @@ def insert_command(conn, phone_number, active, history_log):
         cur.execute("ROLLBACK")
 
 
+# Define function to add a new user and its associated history log to the database
+def add_new_user(phone_number, history_log):
+    with closing(sqlite3.connect(DB_NAME)) as conn:
+        with closing(conn.cursor()) as cursor:
+            # Add new user to the users table
+            cursor.execute(
+                "INSERT INTO users (phone_number, is_active) VALUES (?, 0)",
+                (phone_number,),
+            )
+            user_id = cursor.lastrowid
+
+            # Add new history log to the history table
+            cursor.execute(
+                "INSERT INTO history (user_id, history_log) VALUES (?, ?)",
+                (user_id, history_log),
+            )
+
+            # Commit changes and close the connection
+            conn.commit()
+
+
 if __name__ == "__main__":
     create_db()
+    add_new_user("+16135550166", "User signed up for the app ONE")
+    add_new_user("+16135550185", "User signed up for the app TWO")
+    add_new_user("+16135550118", "User signed up for the app THREE")
