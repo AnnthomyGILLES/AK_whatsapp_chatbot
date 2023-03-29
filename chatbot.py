a1e2e615614ce7b548596e09faa54295fb246ed2
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-03-29 11:18:51 -0400

    Extract chatgpt API

--- chatbot.py
+++ chatbot.py
@@ -8,10 +8,9 @@ import stripe
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from loguru import logger
-from openai.error import RateLimitError
-from ratelimit import sleep_and_retry, limits
 from twilio.rest import Client
 
+from chatgpt_api.chatgpt import ask_chat_conversation
 from mongodb_db import (
     get_user_id_with_phone_number,
     update_history,
@@ -74,60 +73,6 @@ je vous invite à vous abonner dès maintenant. Pour ce faire, veuillez simpleme
 
 Si vous avez des questions ou si vous avez besoin d'aide, n'hésitez pas à me le faire savoir. Je suis là pour vous 
 assister 24h/24 et 7j/7. Alors, commençons notre aventure ensemble ! 🚀"""
-
-
-@sleep_and_retry
-@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-def ask_chat_conversation(message_log):
-    """
-    Send a message to the GPT-3.5-turbo model and return the generated response.
-    This function is rate limited according to the specified limits.
-
-    Args:
-        message_log (list): A list of message dictionaries containing the conversation history.
-
-    Returns:
-        str: The content of the generated message.
-    """
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=message_log,
-            max_tokens=1024,
-            stop=None,
-            temperature=0.7,
-        )
-
-        reply_content = response.choices[0].message.content
-
-        return reply_content
-    except RateLimitError:
-        print("[Log] Rate limit reached")
-
-
-@sleep_and_retry
-@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-def ask_prompt(prompt):
-    """
-    Send a prompt to the text-davinci-003 model and return the generated response.
-    This function is rate limited according to the specified limits.
-
-    Args:
-        prompt (str): The prompt to send to the model.
-
-    Returns:
-        str: The generated text response.
-    """
-    try:
-        response = openai.Completion.create(
-            model="text-davinci-003", prompt=prompt, max_tokens=100, temperature=0.7
-        )
-
-        reply_content = response.choices[0].text
-
-        return reply_content
-    except RateLimitError:
-        print("[Log] Rate limit reached")
 
 
 def append_interaction_to_chat_log(user_id, question):
---
+++ chatgpt_api/chatgpt.py
@@ -0,0 +1,59 @@
+import openai
+from openai.error import RateLimitError
+from ratelimit import sleep_and_retry, limits
+
+from chatbot import MAX_CALLS_PER_MINUTE, ONE_MINUTE
+
+
+@sleep_and_retry
+@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
+def ask_chat_conversation(message_log):
+    """
+    Send a message to the GPT-3.5-turbo model and return the generated response.
+    This function is rate limited according to the specified limits.
+
+    Args:
+        message_log (list): A list of message dictionaries containing the conversation history.
+
+    Returns:
+        str: The content of the generated message.
+    """
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=message_log,
+            max_tokens=1024,
+            stop=None,
+            temperature=0.7,
+        )
+
+        reply_content = response.choices[0].message.content
+
+        return reply_content
+    except RateLimitError:
+        print("[Log] Rate limit reached")
+
+
+@sleep_and_retry
+@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
+def ask_prompt(prompt):
+    """
+    Send a prompt to the text-davinci-003 model and return the generated response.
+    This function is rate limited according to the specified limits.
+
+    Args:
+        prompt (str): The prompt to send to the model.
+
+    Returns:
+        str: The generated text response.
+    """
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003", prompt=prompt, max_tokens=100, temperature=0.7
+        )
+
+        reply_content = response.choices[0].text
+
+        return reply_content
+    except RateLimitError:
+        print("[Log] Rate limit reached")
