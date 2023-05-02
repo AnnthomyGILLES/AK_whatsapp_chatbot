3c0978a43d2438eb19cc31695cc1ed6cc477e375
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-05-01 23:47:08 -0400

    quick fix

--- chatbot.py
+++ chatbot.py
@@ -420,7 +420,7 @@ if __name__ == "__main__":
             host="0.0.0.0",
             port=5000,
             ssl_context=(
-                "/etc/letsencrypt/live/dev.whatia.fr/fullchain.pem",
-                "/etc/letsencrypt/live/dev.whatia.fr/privkey.pem",
+                "/etc/letsencrypt/live/secure.whatia.fr/fullchain.pem",
+                "/etc/letsencrypt/live/secure.whatia.fr/privkey.pem",
             ),
         )
