118664f9b20b3e5cc7bafc88d2fb8a0ceedfb23d
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-05-09 23:17:47 -0400

    clean

--- notifier/send_notification.py
+++ notifier/send_notification.py
@@ -32,45 +32,14 @@ def send_message(body_mess, phone_number, media_url=None):
 
 
 if __name__ == "__main__":
-    # users = UserCollection("users")
-    # list_of_users = users.list_all_users()
-    # for user in list_of_users:
-    #     print(user)
-    #     if user["phone_number"] == "+33667656197":
-    #         send_message("New feature", user["phone_number"])
-    from selenium import webdriver
-    from selenium.webdriver.common.keys import Keys
-    import time
+    ACTIVATION_MESSAGE_FR = """🇫🇷
+    					🎉Bienvenue dans le cercle privilégié des utilisateurs premium de WhatIA! Félicitations! 🎊 \n
+    					Nous sommes ravis de t'accueillir parmi nous et de te proposer un accès privilegié à toutes les fonctionnalités de notre chatbot. Avec ton compte premium, tu es prêt à profiter d'une expérience de qualité supérieure. Seule ton imagination est la limite!💡📱 \n
+    					Que tu souhaites améliorer ton expérience utilisateur ou découvrir de nouvelles fonctionnalités, nous sommes là pour t'accompagner tout au long de ton utilisation. N'hésite donc pas à nous contacter si tu as des questions ou si tu as besoin d'aide. Notre équipe est à ta disposition pour t'offrir une expérience inoubliable sur WhatIA. 🤝👨‍💼 \n\n
 
-    # Replace the following strings with your own information
-    name = "John Smith"  # The name of the contact you want to message
-    message = "Hello, this is a test message."  # The message you want to send
+    					📧 Mail: contact@whatia.fr \n
+    					🔑 Gérer ton abonnement (si abonné): app.whatia.fr/abonnement \n
+    					📷 Instagram (-5% pour les abonnés! Sur demande): https://www.instagram.com/app.whatia.fr  \n\n
 
-    # Open Chrome and go to WhatsApp Web
-    driver = webdriver.Chrome("D:\Téléchargements\chromedriver_win32\chromedriver.exe")
-    driver.get("https://web.whatsapp.com/")
-
-    # Wait for user to log in to WhatsApp Web
-    input(
-        "Press Enter after scanning the QR code on your phone and logging in to WhatsApp Web."
-    )
-
-    # Search for the contact by name and click on it
-    search_box = driver.find_element_by_xpath(
-        '//div[@class="_2_1wd copyable-text selectable-text"]'
-    )
-    search_box.click()
-    search_box.send_keys(name)
-    search_box.send_keys(Keys.RETURN)
-
-    # Wait for the chat to load
-    time.sleep(5)
-
-    # Type the message and send it
-    message_box = driver.find_element_by_xpath('//div[@class="_3uMse"]')
-    message_box.click()
-    message_box.send_keys(message)
-    message_box.send_keys(Keys.RETURN)
-
-    # Close the browser window
-    driver.quit()
+    					Félicitations pour ton choix! Tu ne le regretteras pas, profites de l'expérience! 🚀"""
+    send_message(ACTIVATION_MESSAGE_FR, "+590690976015")
