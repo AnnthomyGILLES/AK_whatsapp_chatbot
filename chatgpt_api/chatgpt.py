86d6f491849059e55826a1cc8201dcbed4683427
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-05-27 12:24:30 -0400

    Fix call

--- chatgpt_api/chatgpt.py
+++ chatgpt_api/chatgpt.py
@@ -5,42 +5,25 @@ from ratelimit import sleep_and_retry, limits
 
 ONE_MINUTE = 60
 MAX_CALLS_PER_MINUTE = 60
-MAX_TOKENS = 400
+MAX_TOKENS = 300
 
 
+@sleep_and_retry
+@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
 async def ask_chat_conversation(prompt):
-    """
-    Send a message to the GPT-3.5-turbo model and return the generated response.
-    This function is rate limited according to the specified limits.
-
-    Args:
-        message_log (list): A list of message dictionaries containing the conversation history.
-
-    Returns:
-        str: The content of the generated message.
-    """
-    async with aiohttp.ClientSession() as session:
-
-        @sleep_and_retry
-        @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-        async def request():
-            try:
-                response = await openai.ChatCompletion.acreate(
-                    model="gpt-3.5-turbo",
-                    messages=prompt,
-                    max_tokens=MAX_TOKENS,
-                    stop=None,
-                    temperature=0.7,
-                    stream=False,
-                )
-
-                reply_content = response.choices[0].message.content
-
-                return reply_content
-            except RateLimitError:
-                print("[Log] Rate limit reached")
-
-        return await request()
+    try:
+        response = await openai.ChatCompletion.acreate(
+            model="gpt-3.5-turbo",
+            messages=prompt,
+            max_tokens=MAX_TOKENS,
+            stop=None,
+            temperature=0.7,
+            stream=False,
+        )
+        reply_content = response.choices[0].message.content
+        return reply_content
+    except RateLimitError:
+        print("[Log] Rate limit reached")
 
 
 async def ask_prompt(prompt):
