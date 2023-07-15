5b32008ee4a76f8c5ef08a1777109d95e4f30633
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-07-15 12:36:44 -0400

    remove unecessary function

--- chatgpt_api/chatgpt.py
+++ chatgpt_api/chatgpt.py
@@ -23,37 +23,3 @@ async def ask_chat_conversation(prompt, max_tokens=350):
         return reply_content
     except RateLimitError:
         print("[Log] Rate limit reached")
-
-
-async def ask_prompt(prompt):
-    """
-    Send a prompt to the text-davinci-002 model and return the generated response.
-    This function is rate limited according to the specified limits.
-
-    Args:
-        prompt (str): The prompt to send to the model.
-
-    Returns:
-        str: The generated text response.
-    """
-    async with aiohttp.ClientSession() as session:
-
-        @sleep_and_retry
-        @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-        async def request():
-            try:
-                response = await openai.Completion.create(
-                    engine="text-davinci-003",
-                    prompt=prompt,
-                    max_tokens=MAX_TOKENS,
-                    temperature=0.7,
-                    stream=False,
-                )
-
-                reply_content = response.choices[0].text
-
-                return reply_content
-            except RateLimitError:
-                print("[Log] Rate limit reached")
-
-        return await request()
