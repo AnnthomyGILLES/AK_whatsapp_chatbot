a0afe7c6635e93f61110be506d86a5076529d5e3
AnnthomyGILLES <annthomy.gilles@gmail.com>
2023-12-24 12:32:11 -0500

    Clear api key

--- chatgpt_api/test_chatgpt.py
+++ chatgpt_api/test_chatgpt.py
@@ -1,7 +1,6 @@
 import asyncio
 from pathlib import Path
 
-import openai
 import pytest
 from dotenv import load_dotenv
 
@@ -9,7 +8,6 @@ from chatgpt_api.chatgpt import ask_chat_conversation
 
 load_dotenv(dotenv_path=Path("..", ".env.development"))
 # OpenAI Chat GPT
-openai.api_key = "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"
 
 
 @pytest.mark.asyncio
